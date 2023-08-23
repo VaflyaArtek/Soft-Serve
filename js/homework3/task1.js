e07f9a5116d8cf1b57793b65a11bd295613ab85b
VaflyaArtek <yuriy.shpak.2017@gmail.com>
2023-08-23 14:58:21 +0300

    structure

---
+++ js/homework3/task1.js
@@ -0,0 +1,18 @@
+// task 1 
+const createArray = (start, end) => {
+    const result = [];
+    
+    if (start > end) {
+        for (let i = start; i >= end; i--) {
+            result.push(i);
+        } 
+    } else {
+        for (let i = start; i <= end; i++) {
+            result.push(i);
+        }
+    }
+    return result; 
+};
+
+console.log(createArray(2, 7)); // [2, 3, 4, 5, 6, 7]
+console.log(createArray(7, 2)); // [7, 6, 5, 4, 3, 2]
---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.addEventListener('partialsLoaded', () => {
+    import('./homework3/task1.js');
+    import('./homework3/task2.js');
+});
